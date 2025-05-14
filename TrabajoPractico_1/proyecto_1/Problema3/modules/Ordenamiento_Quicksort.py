b79dfe4894a8c34c5b5e2f7e1ee3e09ba5d0e12c
kiaara06 <kiarastrickerks2006@gmail.com>
2025-05-14 10:41:31 -0300

    acomodamos el orden de los modulos

--- TrabajoPractico_1/proyecto_1/Problema3/modules/Ordenamiento_Quicksort.py
+++
@@ -1,8 +0,0 @@
-def ordenamiento_Quicksort(lista):
-    if len(lista) <= 1: 
-        return lista
-    else:
-        pivote = lista[0] 
-        menores = [x for x in lista[1:] if x < pivote] 
-        mayores = [x for x in lista[1:] if x >= pivote] 
-        return ordenamiento_Quicksort(menores) + [pivote] + ordenamiento_Quicksort(mayores)
